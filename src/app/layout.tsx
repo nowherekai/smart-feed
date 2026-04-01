d72017de4d8281e9f8f49e981f5ff91a37c2b675
kailaichao <klc1989110@gmail.com>
2026-04-01 09:11:22 +0800

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import type { ReactNode } from "react";
 
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "smart-feed",
@@ -14,7 +18,7 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="zh-CN">
+    <html lang="zh-CN" className={cn("font-sans", geist.variable)}>
       <body>{children}</body>
     </html>
   );
